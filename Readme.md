0c8a522b339a5e8df21f849f6e0b738966491049
theindigamer <theindigamer15@gmail.com>
2018-07-09 19:31:26 -0400

    Add License and Readme files.

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# yolexo: You Only LEX Once
+
+To-be lexer generator.
+
+## License
+
+BSD-3-Clause, see the License file.
+
+## Trivia
+
+I wanted to use the name `yolo` but that was already taken by another crate.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
+extern crate regex_syntax;
+
 #[cfg(test)]
 mod tests {
+    extern crate regex_syntax;
+    use regex_syntax::ast::parse::Parser as P;
+    use regex_syntax::hir::translate::Translator;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let a = P::new().parse(r"a|b");
+        println!("{:?}", a);
+        assert_eq!(1 + 2, 4);
     }
 }
